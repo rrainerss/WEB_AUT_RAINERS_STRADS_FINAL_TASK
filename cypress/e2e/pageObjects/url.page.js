bd0cbe189a732343f4af725b35e581171a74f5dc
rrainerss <rainersstrads8@gmail.com>
2024-06-04 00:25:25 +0300

    Added comment

--- cypress/e2e/pageObjects/url.page.js
+++ cypress/e2e/pageObjects/url.page.js
@@ -2,6 +2,7 @@
 
 export default class Url {
     visitForm() {
+        //I know theres a cypress config file for URLs but this makes more sense to me
         cy.visit('https://demoqa.com/automation-practice-form');
     }
 }
